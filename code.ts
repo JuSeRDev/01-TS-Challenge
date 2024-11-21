a4723ef1d4d41e2b3bec50fab59a30350c581982
JuSeRDev <juserdev@gmail.com>
2024-11-21 15:31:28 -0500

    correcion de asignaciones con el DOM

--- code.ts
+++ code.ts
@@ -1,25 +1,20 @@
-const button = document.querySelector(".button") as HTMLInputElement
-
+const button = document.querySelector(".button") as HTMLButtonElement
 const cardName = document.getElementById("cardholder-name") as HTMLInputElement
-const nameCard = document.querySelector(".name-card") as HTMLInputElement
-
+const nameCard = document.querySelector(".name-card") as HTMLParagraphElement
 
 cardName?.addEventListener("input", ()=>{
     let name:string = cardName.value.slice(0, 16)
-    nameCard!.innerHTML = name || "JANE APPLESSED"
+    nameCard.innerHTML = name || "JANE APPLESSED"
     cardName.value = name
 })
 
-const numberCard = document.querySelector(".number-card") as HTMLInputElement
+const numberCard = document.querySelector(".number-card") as HTMLParagraphElement
 const numberInput = document.getElementById("card-number") as HTMLInputElement
-
-const monthCard = document.querySelector(".m") as HTMLInputElement
+const monthCard = document.querySelector(".m") as HTMLParagraphElement
 const monthInput = document.querySelector(".mm") as HTMLInputElement
-
-const yearCard = document.querySelector(".y") as HTMLInputElement
+const yearCard = document.querySelector(".y") as HTMLParagraphElement
 const yearInput = document.querySelector(".yy") as HTMLInputElement
-
-const cvcCard = document.querySelector(".card-cvc") as HTMLInputElement
+const cvcCard = document.querySelector(".card-cvc") as HTMLParagraphElement
 const cvcInput = document.getElementById("cvc") as HTMLInputElement
 
 const transform = ( inputElement:HTMLInputElement, character:number, print:HTMLElement, start: string)=>{
@@ -29,7 +24,7 @@ const transform = ( inputElement:HTMLInputElement, character:number, print:HTMLE
         input = input.slice(0, character)
         let formatted: string = input.match(/.{1,4}/g)?.join(" ") || input
         inputElement.value = formatted
-        print!.innerHTML = formatted || start
+        print.innerHTML = formatted || start
     })
 }
 
@@ -37,5 +32,4 @@ transform(numberInput, 16, numberCard, "0000 0000 0000 0000")
 transform(monthInput, 2, monthCard, "00")
 transform(yearInput, 2, yearCard, "00")
 transform(cvcInput, 3, cvcCard, "000")
-
 
